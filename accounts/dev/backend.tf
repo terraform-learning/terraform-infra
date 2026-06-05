7e436fdcf1babf29f2d1e7f4425216fb47d53073
ramya8812 <itzmeramyaarumugam@gmail.com>
2026-06-05 16:05:08 +0800

    dev-role-creation

--- accounts/dev/backend.tf
+++ accounts/dev/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "ramya-s3-bucket-tfstate"
     key            = "dev/terraform.tfstate"
-    region         = "us-east-1"
+    region         = "us-east-2"
     dynamodb_table = "digi-dev-tf-lock-state"
   }
 }
